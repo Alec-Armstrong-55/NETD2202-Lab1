a03d22c9036f0ddb09fbc25001ba5a6a815ec683
Alec-Armstrong-55 <60987510+Alec-Armstrong-55@users.noreply.github.com>
2020-02-19 16:27:34 -0500

    Add files via upload

---
+++ NETD2202LAB1/Lab1/Lab1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmAverageUnitsShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnEnterUseable = New System.Windows.Forms.Button()
+        Me.btnResetForm = New System.Windows.Forms.Button()
+        Me.btnExitForm = New System.Windows.Forms.Button()
+        Me.txtGetUnits = New System.Windows.Forms.TextBox()
+        Me.txtShowPrevUnits = New System.Windows.Forms.TextBox()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.ttpAverageUnitsShippedForm = New System.Windows.Forms.ToolTip(Me.components)
+        Me.lblShowAverage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnEnterUseable
+        '
+        Me.btnEnterUseable.BackColor = System.Drawing.Color.White
+        Me.btnEnterUseable.Location = New System.Drawing.Point(68, 552)
+        Me.btnEnterUseable.Name = "btnEnterUseable"
+        Me.btnEnterUseable.Size = New System.Drawing.Size(153, 75)
+        Me.btnEnterUseable.TabIndex = 5
+        Me.btnEnterUseable.Text = "&Enter"
+        Me.ttpAverageUnitsShippedForm.SetToolTip(Me.btnEnterUseable, "Assign the units entered to the day of the week.")
+        Me.btnEnterUseable.UseVisualStyleBackColor = False
+        '
+        'btnResetForm
+        '
+        Me.btnResetForm.BackColor = System.Drawing.Color.White
+        Me.btnResetForm.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnResetForm.Location = New System.Drawing.Point(326, 552)
+        Me.btnResetForm.Name = "btnResetForm"
+        Me.btnResetForm.Size = New System.Drawing.Size(153, 75)
+        Me.btnResetForm.TabIndex = 6
+        Me.btnResetForm.Text = "&Reset"
+        Me.ttpAverageUnitsShippedForm.SetToolTip(Me.btnResetForm, "Reset the form to day one.")
+        Me.btnResetForm.UseVisualStyleBackColor = False
+        '
+        'btnExitForm
+        '
+        Me.btnExitForm.BackColor = System.Drawing.Color.White
+        Me.btnExitForm.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExitForm.Location = New System.Drawing.Point(584, 552)
+        Me.btnExitForm.Name = "btnExitForm"
+        Me.btnExitForm.Size = New System.Drawing.Size(153, 75)
+        Me.btnExitForm.TabIndex = 7
+        Me.btnExitForm.Text = "E&xit"
+        Me.ttpAverageUnitsShippedForm.SetToolTip(Me.btnExitForm, "Exit the form.")
+        Me.btnExitForm.UseVisualStyleBackColor = False
+        '
+        'txtGetUnits
+        '
+        Me.txtGetUnits.BackColor = System.Drawing.Color.White
+        Me.txtGetUnits.Location = New System.Drawing.Point(331, 70)
+        Me.txtGetUnits.MaxLength = 4
+        Me.txtGetUnits.Name = "txtGetUnits"
+        Me.txtGetUnits.Size = New System.Drawing.Size(100, 38)
+        Me.txtGetUnits.TabIndex = 1
+        Me.ttpAverageUnitsShippedForm.SetToolTip(Me.txtGetUnits, "Enter the number of units shipped.")
+        '
+        'txtShowPrevUnits
+        '
+        Me.txtShowPrevUnits.BackColor = System.Drawing.Color.White
+        Me.txtShowPrevUnits.Location = New System.Drawing.Point(222, 143)
+        Me.txtShowPrevUnits.Multiline = True
+        Me.txtShowPrevUnits.Name = "txtShowPrevUnits"
+        Me.txtShowPrevUnits.ReadOnly = True
+        Me.txtShowPrevUnits.Size = New System.Drawing.Size(360, 300)
+        Me.txtShowPrevUnits.TabIndex = 3
+        Me.txtShowPrevUnits.TabStop = False
+        Me.ttpAverageUnitsShippedForm.SetToolTip(Me.txtShowPrevUnits, "Shows the units shipped on previous days.")
+        '
+        'lblUnits
+        '
+        Me.lblUnits.AutoSize = True
+        Me.lblUnits.Location = New System.Drawing.Point(237, 73)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(88, 32)
+        Me.lblUnits.TabIndex = 0
+        Me.lblUnits.Text = "&Units:"
+        Me.ttpAverageUnitsShippedForm.SetToolTip(Me.lblUnits, "Enter the number of units shipped.")
+        '
+        'lblDay
+        '
+        Me.lblDay.AutoSize = True
+        Me.lblDay.Location = New System.Drawing.Point(455, 73)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(0, 32)
+        Me.lblDay.TabIndex = 2
+        Me.ttpAverageUnitsShippedForm.SetToolTip(Me.lblDay, "Specifies the day of the week.")
+        '
+        'lblShowAverage
+        '
+        Me.lblShowAverage.BackColor = System.Drawing.Color.LightGray
+        Me.lblShowAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblShowAverage.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.lblShowAverage.Location = New System.Drawing.Point(222, 457)
+        Me.lblShowAverage.Name = "lblShowAverage"
+        Me.lblShowAverage.Size = New System.Drawing.Size(360, 60)
+        Me.lblShowAverage.TabIndex = 4
+        Me.lblShowAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ttpAverageUnitsShippedForm.SetToolTip(Me.lblShowAverage, "Displays the average units shipped over the seven days.")
+        '
+        'frmAverageUnitsShipped
+        '
+        Me.AcceptButton = Me.btnEnterUseable
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.CancelButton = Me.btnResetForm
+        Me.ClientSize = New System.Drawing.Size(799, 693)
+        Me.Controls.Add(Me.lblShowAverage)
+        Me.Controls.Add(Me.lblDay)
+        Me.Controls.Add(Me.lblUnits)
+        Me.Controls.Add(Me.txtShowPrevUnits)
+        Me.Controls.Add(Me.txtGetUnits)
+        Me.Controls.Add(Me.btnExitForm)
+        Me.Controls.Add(Me.btnResetForm)
+        Me.Controls.Add(Me.btnEnterUseable)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmAverageUnitsShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnEnterUseable As Button
+    Friend WithEvents btnResetForm As Button
+    Friend WithEvents btnExitForm As Button
+    Friend WithEvents txtGetUnits As TextBox
+    Friend WithEvents txtShowPrevUnits As TextBox
+    Friend WithEvents lblUnits As Label
+    Friend WithEvents lblDay As Label
+    Friend WithEvents ttpAverageUnitsShippedForm As ToolTip
+    Friend WithEvents lblShowAverage As Label
+End Class
---
+++ NETD2202LAB1/Lab1/Lab1.vb
@@ -0,0 +1,169 @@
+﻿'Project: Lab1
+'Name: Alec Armstrong 100617553
+'Date: 2020-01-16
+'Desc: Will take an input for each day in the week numbered from 1 to 7. Input data will then be validated and 
+'displayed to the user. Once the number of units has been entered for the final day a calculation for average will 
+'be done and displayed to the user. After the 7th day will not allow the user to access the enter key or input any
+'more data until the form is reset. The user will be allowed to exit at any time.
+
+Option Strict On
+
+Public Class frmAverageUnitsShipped
+
+    'Constant
+    'Days in the week
+    Const DAYS_IN_WEEK As Integer = 7
+
+    'Variables
+    'Day counter to keep track of day in week
+    Dim DayCounter As Integer = 1
+    'Average, double
+    Dim AverageUnitsShipped As Double = 0.0
+    'Tester used to check if number is a whole number, integer
+    Dim TryParseTesterInteger As Integer = 0
+    'Tester used to check if number is a real number, double
+    Dim TryParseTesterDouble As Double = 0.0
+
+    'Array
+    'Set to contain unit entries for each day, using days in the week constant, integers
+    Dim UnitsShipped(DAYS_IN_WEEK - 1) As Integer
+
+    'Initialize variables, array and day display on form load
+    Private Sub frmAverageUnitsShipped_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Reset variables
+        'Day counter to keep track of day in week
+        DayCounter = 1
+        'Average
+        AverageUnitsShipped = 0.0
+        'Testers
+        TryParseTesterInteger = 0
+        TryParseTesterDouble = 0.0
+
+        'Array
+        'Set to contain unit entries for each day, using days in the week constant, integers
+        ReDim UnitsShipped(DAYS_IN_WEEK - 1)
+
+        'Day display
+        lblDay.Text = "Day: " & CType(DayCounter, String)
+
+    End Sub
+
+    'Exit button
+    Private Sub btnExitForm_Click(sender As Object, e As EventArgs) Handles btnExitForm.Click
+
+        'Close the form
+        Me.Close()
+
+    End Sub
+
+    'Reset button
+    Private Sub btnResetForm_Click(sender As Object, e As EventArgs) Handles btnResetForm.Click
+
+        'Clear all fields 
+        txtGetUnits.Clear()
+        lblShowAverage.Text = ""
+        txtShowPrevUnits.Clear()
+
+        'Clear contents of array and variables
+        ReDim UnitsShipped(DAYS_IN_WEEK - 1)
+        AverageUnitsShipped = 0.0
+        TryParseTesterInteger = 0
+        TryParseTesterDouble = 0.0
+        DayCounter = 1
+
+        'Set focus on the input field and reset all tools
+        lblDay.Text = "Day: " & CType(DayCounter, String)
+        btnEnterUseable.Enabled = True
+        btnEnterUseable.BackColor = Color.White
+        txtGetUnits.ReadOnly = False
+        txtGetUnits.BackColor = Color.White
+        txtGetUnits.Focus()
+
+    End Sub
+
+    'Enter button
+    Private Sub btnEnterUseable_Click(sender As Object, e As EventArgs) Handles btnEnterUseable.Click
+
+        'Validate that input is an integer and in range
+        If Integer.TryParse(txtGetUnits.Text, TryParseTesterInteger) = True And TryParseTesterInteger >= 0 And TryParseTesterInteger <= 5000 Then
+
+            'Check what day of the week it is
+            If DayCounter <= 6 Then
+
+                'If days 1-6 store units in day array and display in the PrevUnits text box, reset integer tester
+                UnitsShipped(DayCounter - 1) = TryParseTesterInteger
+                txtShowPrevUnits.Text = txtShowPrevUnits.Text + CType(UnitsShipped(DayCounter - 1), String) + vbNewLine
+                txtGetUnits.Clear()
+                TryParseTesterInteger = 0
+
+                'Update day counter and display
+                DayCounter = DayCounter + 1
+                lblDay.Text = "Day: " & CType(DayCounter, String)
+
+            ElseIf DayCounter = 7 Then
+
+                'If day 7 store units in day array, display in the PrevUnits text box 
+                'Also make the enter button hidden and get units field and show the unuseable enter button and unusable units label
+                'Update day counter
+                UnitsShipped(DayCounter - 1) = TryParseTesterInteger
+                txtShowPrevUnits.Text = txtShowPrevUnits.Text + CType(UnitsShipped(DayCounter - 1), String)
+                txtGetUnits.Clear()
+                TryParseTesterInteger = 0
+                DayCounter = DayCounter + 1
+                btnEnterUseable.Enabled = False
+                btnEnterUseable.BackColor = Color.LightGray
+                txtGetUnits.ReadOnly = True
+                txtGetUnits.BackColor = Color.LightGray
+
+                'Calculate the average of all the days in the array
+                For AverageCounter As Integer = 1 To DAYS_IN_WEEK
+
+                    AverageUnitsShipped = AverageUnitsShipped + UnitsShipped(AverageCounter - 1)
+
+                Next
+
+                'Display the average of all the days
+                AverageUnitsShipped = AverageUnitsShipped / DAYS_IN_WEEK
+                lblShowAverage.Text = "Average Per Day: " & CType(Math.Round(AverageUnitsShipped, 2), String)
+
+            Else
+
+                'Do nothing until reset button is used
+
+            End If
+
+        Else
+
+            If Double.TryParse(txtGetUnits.Text, TryParseTesterDouble) = False Then
+
+                'Display must be a number error
+                MessageBox.Show("Please enter a numeric value.")
+                txtGetUnits.Clear()
+                txtGetUnits.Focus()
+
+            Else
+
+                If TryParseTesterDouble < 0 Or TryParseTesterDouble > 5000 Then
+
+                    'Display must be in range number error
+                    MessageBox.Show("Please enter a numeric value between 0 and 5000.")
+                    txtGetUnits.Clear()
+                    txtGetUnits.Focus()
+
+                Else
+
+                    'Display must be a integer error
+                    MessageBox.Show("Please enter a whole numeric value.")
+                    txtGetUnits.Clear()
+                    txtGetUnits.Focus()
+
+                End If
+
+            End If
+
+        End If
+
+    End Sub
+
+End Class
